c158674dcbfa214e098a6a11f14a45fa59a8e989
박성원 <skfhddlg@naver.com>
2023-10-01 07:21:11 +0900

    코드정리, 일괄설명(태그)추가, 사진태그별로 분류하는 기능 추가

---
+++ BlazorForm.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
+
+namespace GooglePhotoExplorer;
+
+file static class Util
+{
+    public static T As<T>(this T t, Action<T> a) { a(t); return t; }
+}
+
+class BlazorForm<T> : BlazorForm where T : ComponentBase
+{
+    public BlazorForm() : base(typeof(T)) { }
+    public object? this[Expression<Func<T, object>> selectMember]
+    {
+        init => base[selectMember.Body switch
+        {
+            UnaryExpression { Operand: MemberExpression { Member.Name: string key } } => key, //박싱이 발생할경우
+            MemberExpression { Member.Name: string key } => key,
+            _ => throw new("Expression Err")
+        }] = value;
+    }
+}
+class BlazorForm : Form
+{
+    public BlazorForm(Type t) => Controls.Add(new BlazorWebView
+    {
+        Dock = DockStyle.Fill,
+        HostPage = "wwwroot/index.html",
+        RootComponents = { new("#app", t, parameters) },
+        Services = Services,
+    });
+    readonly Dictionary<string, object?> parameters = new();
+    public object? this[string key] { init => parameters[key] = value; }
+    static readonly ServiceProvider Services = new ServiceCollection().As(s =>
+    {
+        s.AddBlazorWebViewDeveloperTools(); //F12누르면 개발자도구
+        s.AddWindowsFormsBlazorWebView();
+        s.AddSingleton<IConfiguration>(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build());
+        s.AddGooglePhotos();
+    }).BuildServiceProvider();
+}
